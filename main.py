e7a008350b942c0f327ad3f31b8336c0e6f84338
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-07-28 08:37:46 -0300

    add if not exists to table creation

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ se nao mude a variavel e de run
 os povoamentos colocados sao mais de exemplo dps a gente deixa mais completo
 '''
 
-rebooted = False
+rebooted = True
 
 if rebooted:
     cursor.execute("""
-    CREATE TABLE pessoa (
+    CREATE TABLE IF NOT EXISTS  pessoa (
     cpf  CHAR(11)        NOT NULL,
     nome VARCHAR2(100)   NOT NULL,
     CONSTRAINT pk_pessoa PRIMARY KEY (cpf)
@@ -26,7 +26,7 @@ if rebooted:
     """)
 
     cursor.execute("""
-    CREATE TABLE telefone (
+    CREATE TABLE IF NOT EXISTS  telefone (
     pessoa_fk CHAR(11)        NOT NULL,
     telefone  VARCHAR2(20)    NOT NULL,
     CONSTRAINT pk_telefone PRIMARY KEY (pessoa_fk, telefone),
@@ -37,7 +37,7 @@ if rebooted:
     """)
 
     cursor.execute("""
-    CREATE TABLE socio (
+    CREATE TABLE IF NOT EXISTS  socio (
     cpf  CHAR(11)      NOT NULL,
     cartao  VARCHAR2(16)  NOT NULL,
     CONSTRAINT pk_socio PRIMARY KEY (cpf),
@@ -48,7 +48,7 @@ if rebooted:
     """)
 
     cursor.execute("""
-    CREATE TABLE funcionario (
+    CREATE TABLE IF NOT EXISTS  funcionario (
     cpf          CHAR(11)        NOT NULL,
     salario      NUMBER(12,2)    NOT NULL,
     cargo      VARCHAR(100)    NOT NULL,
@@ -97,7 +97,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE biblioteca (
+    CREATE TABLE IF NOT EXISTS  biblioteca (
     id  VARCHAR2(10)        NOT NULL,
     local_estado VARCHAR2(100)   NOT NULL,
     local_cep VARCHAR2(8)   NOT NULL,
@@ -107,7 +107,7 @@ if rebooted:
     """)
 
     cursor.execute("""
-    CREATE TABLE cargo_comissionado (
+    CREATE TABLE IF NOT EXISTS  cargo_comissionado (
     cargo_comissionado VARCHAR2(100)  NOT NULL,
     gratificacao NUMBER(5, 2)   NOT NULL,
     CONSTRAINT pk_cargo_com PRIMARY KEY (cargo_comissionado)
@@ -131,7 +131,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE livro (
+    CREATE TABLE IF NOT EXISTS  livro (
     ISBN  VARCHAR2(13)        NOT NULL,
     nome VARCHAR2(100)   NOT NULL,
     ano NUMBER(5)   NOT NULL,
@@ -151,7 +151,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE autor (
+    CREATE TABLE IF NOT EXISTS  autor (
     id  VARCHAR2(12)        NOT NULL,
     nome VARCHAR2(100)   NOT NULL,
     CONSTRAINT pk_autor PRIMARY KEY (id)
@@ -159,7 +159,7 @@ if rebooted:
     """)
 
     cursor.execute("""
-    CREATE TABLE colecao (
+    CREATE TABLE IF NOT EXISTS  colecao (
     id  VARCHAR2(12)        NOT NULL,
     nome VARCHAR2(100)   NOT NULL,
     CONSTRAINT pk_colecao PRIMARY KEY (id)
@@ -182,7 +182,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE pertence (
+    CREATE TABLE IF NOT EXISTS  pertence (
     ISBN  VARCHAR2(13)        NOT NULL,
     id  VARCHAR2(12)        NOT NULL,
     CONSTRAINT pk_pertence PRIMARY KEY (ISBN, id),
@@ -210,7 +210,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE escreve (
+    CREATE TABLE IF NOT EXISTS  escreve (
     ISBN  VARCHAR2(13)        NOT NULL,
     id  VARCHAR2(12)        NOT NULL,
     CONSTRAINT pk_pertence PRIMARY KEY (ISBN, id),
@@ -235,7 +235,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE demanda (
+    CREATE TABLE IF NOT EXISTS  demanda (
     ISBN  VARCHAR2(13)        NOT NULL,
     id  VARCHAR2(10)        NOT NULL,
     cpf CHAR(11) NOT NULL,
@@ -261,7 +261,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-        CREATE TABLE secao (
+        CREATE TABLE IF NOT EXISTS  secao (
         id_biblioteca CHAR(10) NOT NULL,
         codigo CHAR(10) NOT NULL,
         descricao VARCHAR2(100) NOT NULL,
@@ -282,7 +282,7 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-        CREATE TABLE exemplar (
+        CREATE TABLE IF NOT EXISTS  exemplar (
         id_biblioteca CHAR(10) NOT NULL,
         codigo NUMBER(10) NOT NULL,
         ISBN VARCHAR2(13) NOT NULL,
@@ -294,6 +294,9 @@ if rebooted:
             ON DELETE CASCADE,
         CONSTRAINT fk_secao FOREIGN KEY (id_biblioteca_from_secao, codigo_secao)
             REFERENCES secao (id_biblioteca, codigo)
+            ON DELETE CASCADE
+        CONSTRAINT fk_isbn FOREIGN KEY(ISBN)
+            REFERENCES livro(ISBN)
             ON DELETE CASCADE
         );
     """)
@@ -312,15 +315,15 @@ if rebooted:
     conn.commit()
 
     cursor.execute("""
-    CREATE TABLE emprestimo (
+    CREATE TABLE IF NOT EXISTS  emprestimo (
       id NUMBER(10) PRIMARY KEY,
       cpf_socio CHAR(11),
       data DATE,
       prazo DATE,
       id_biblioteca CHAR(10),
       codigo_exemplar NUMBER(10),
-      FOREIGN KEY (cpf_socio) REFERENCES SOCIO(CPF),
-      FOREIGN KEY (id_biblioteca, codigo_exemplar) REFERENCES EXEMPLAR(id_biblioteca, codigo)
+      CONSTRAINT fk_emprestimo_socio FOREIGN KEY (cpf_socio) REFERENCES SOCIO(CPF),
+      CONSTRAINT fk_emprestimo_exemplar FOREIGN KEY (id_biblioteca, codigo_exemplar) REFERENCES EXEMPLAR(id_biblioteca, codigo)
     );
     """)
 
